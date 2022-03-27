95f10bf3b7dcc67c6e9a8cb3ae8a73117e083058
VEighteen <83343635+VEighteen@users.noreply.github.com>
2022-03-27 19:43:04 +0300

    Пустой контроллер

---
+++ News/Controllers/PagesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using News.Db;
+
+namespace News.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PagesController : ControllerBase
+    {
+        private readonly ILogger<PagesController> _logger;
+        private readonly DataActions _dataActions;
+
+        public PagesController(ILogger<PagesController> logger)
+        {
+            _logger = logger;
+            _dataActions = new DataActions(new Connection());
+        }
+    }
+
+}
