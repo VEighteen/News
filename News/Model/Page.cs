34f42ecbee28a9a078b8627ab59ec07b857b23a2
VEighteen <83343635+VEighteen@users.noreply.github.com>
2022-03-27 17:02:16 +0300

    Добавлена модель страницы, поиск по слову на странице, поиск сущностией на странице

---
+++ News/Model/Page.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Pullenti;
+using Pullenti.Ner;
+
+namespace News.Model
+{
+    public class Page
+    {
+        [Key]
+        public int ID { get; set; }
+
+        public string Html { get; set; } 
+        
+        [Required]
+        public string Url { get; set; }
+
+        public string Text { get; set; }
+        
+        public DateTime Date { get; set; }
+
+        public Page FindPageByWord(string text)
+        {
+            return Text.Contains(text) ? this : null;
+        }
+
+        public IEnumerable<Referent> FindEntities()
+        {
+            Sdk.InitializeAll();
+
+            using (Processor processor = ProcessorService.CreateProcessor())
+            {
+                return processor.Process(new SourceOfAnalysis(Text)).Entities;
+            }
+
+        }
+    }
+
+}
