2179df1d93d78a47ab45821de9cd4fad0d5ce045
VEighteen <83343635+VEighteen@users.noreply.github.com>
2022-03-27 18:40:21 +0300

    Класс подключения

---
+++ News/Db/Connection.cs
@@ -0,0 +1,22 @@
+﻿using ServiceStack.OrmLite;
+using System.Data;
+
+namespace News.Db
+{
+    public class Connection
+    {
+        private readonly IConfiguration configuration;
+
+        private readonly OrmLiteConnectionFactory _dbFactory ;
+
+        public Connection()
+        {
+             _dbFactory = new OrmLiteConnectionFactory(configuration.GetConnectionString("DevConnection"), SqlServerDialect.Provider);
+        }
+
+        public IDbConnection getConnection()
+        {
+            return _dbFactory.Open();
+        }
+    }
+}
